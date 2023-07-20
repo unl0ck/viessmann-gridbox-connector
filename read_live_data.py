d9c88c958123881c0287b9dd8951b1a7546a9507
unl0ck <sa_helming@icloud.com>
2023-07-20 14:41:32 +0200

    Generate a Class first steps to generate a Service

--- read_live_data.py
+++ read_live_data.py
@@ -1,5 +1,7 @@
 import json
 import requests
+#import paho.mqtt.client as mqtt
+#import ha-mqtt-discoverable
 # Opening JSON file
 f = open('config.json')
   
@@ -8,20 +10,39 @@ f = open('config.json')
 data = json.load(f)
 f.close() 
 
-response = requests.post(data["urls"]["login"], data["login"])
-#access_token = response.content["access_token"]
-response_json = response.json()
-print(response_json)
-id_token = response_json["id_token"]
-gateway_url = data["urls"]["gateways"]
+class GridboxConnector:
+    id_token = ""
 
-headers = {"Authorization": "Bearer {}".format(id_token)}
-response = requests.get(gateway_url,headers=headers)
-response_json = response.json()
-gateway = response_json[0]
-gateway_id = gateway["system"]["id"]
-live_url = data["urls"]["live"]
-live_url = live_url.format(gateway_id)
-response = requests.get(live_url,headers=headers)
-response_json = response.json()
-print(response_json)
+    def __init__(self,config):
+        self.login_url = config["urls"]["login"]
+        self.login_body = config["login"]
+        self.gateway_url = config["urls"]["gateways"]
+        self.live_url = config["urls"]["live"]
+        self.get_token()
+        self.generate_header()
+        self.get_gateway_id()
+
+    def get_token(self):
+        response = requests.post(self.login_url, self.login_body)
+        response_json = response.json()
+        self.id_token = response_json["id_token"]
+    
+    def generate_header(self):
+        self.headers = {"Authorization": "Bearer {}".format(self.id_token)}
+
+    def get_gateway_id(self):
+        response = requests.get(self.gateway_url,headers=self.headers)
+        response_json = response.json()
+        gateway = response_json[0]
+        self.gateway_id = gateway["system"]["id"]
+    
+    def retrieve_live_data(self):
+        response = requests.get(self.live_url.format(self.gateway_id),headers=self.headers)
+        if response.status_code == 200:
+            response_json = response.json()
+            print(response_json)
+        else:
+            self.get_token()
+            self.retrieve_live_data(self)
+
+GridboxConnector(data).retrieve_live_data()
