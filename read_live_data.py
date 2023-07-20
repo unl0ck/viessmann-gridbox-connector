57588d80633e05719f8b18f5051e366589a20e31
unl0ck <sa_helming@icloud.com>
2023-07-20 08:57:34 +0200

    Update read_live_data.py

--- read_live_data.py
+++ read_live_data.py
@@ -11,8 +11,17 @@ f.close()
 response = requests.post(data["urls"]["login"], data["login"])
 #access_token = response.content["access_token"]
 response_json = response.json()
+print(response_json)
 id_token = response_json["id_token"]
-response = requests.get(data["urls"]["gateways")
-response_json = response.json()
+gateway_url = data["urls"]["gateways"]
 
+headers = {"Authorization": "Bearer {}".format(id_token)}
+response = requests.get(gateway_url,headers=headers)
+response_json = response.json()
+gateway = response_json[0]
+gateway_id = gateway["system"]["id"]
+live_url = data["urls"]["live"]
+live_url = live_url.format(gateway_id)
+response = requests.get(live_url,headers=headers)
+response_json = response.json()
 print(response_json)
