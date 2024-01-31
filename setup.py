820961ccc92c748fe3a5b75b54e20f53c014dd58
unl0ck <sa_helming@icloud.com>
2024-01-31 07:15:35 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='viessmann-gridbox-connector',
     version='1.0',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     packages=find_packages(),
     install_requires=[
         'requests',
