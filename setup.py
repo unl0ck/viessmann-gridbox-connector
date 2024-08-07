3a85bec7dcdbdfa0ab450f5b154393076dc0a3eb
unl0ck <unl0ck@users.noreply.github.com>
2024-08-07 05:25:19 +0000

    Bump version: 1.4.1 -> 1.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='viessmann-gridbox-connector',
-    version='1.4.1',
+    version='1.5.0',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     packages=find_packages(),
